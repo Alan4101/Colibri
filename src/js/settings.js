a3082ebb99c68cd8767eede2816b02be1fd89d41
Andrew Tolochkevych <andriy410tolochkevitch@gmail.com>
2020-06-20 16:48:13 +0300

    add POST req in setting js

--- src/js/settings.js
+++ src/js/settings.js
@@ -2,10 +2,41 @@ const btnOnSubmit = document.getElementById("btnOnSubmit");
 const contactForm = document.forms.contactForm;
 
 
-btnOnSubmit.addEventListener("click", function(){
+btnOnSubmit.addEventListener("click", function(e){
+  e.preventDefault();
+  
+  var body{
+    name :    contactForm.contactName.value,
+    email :   contactForm.contactEmail.value,
+    subject : contactForm.contactSubject.value,
+    message : contactForm.contactMessage.value;
+  }
+  if(name!= "" && email!= "" && subject!= "" && message!= ""){
+    try {
+      const response = await fetch('send.php', {
+      method: 'POST',
+      body: JSON.stringify(body), 
+      headers: {
+      ' Content-Type': 'application/json'
+      }
+    });
+    const json = await response.json();
+    console.log('Succes:', JSON.stringify(json));
+    
+    } catch (error) {
+      console.error('Big Error:', error);
+    }
+    reset();
+  }esle{
+    console.log("some fiel is empty!")
+  }
+   
+},false)
+
+
+function resetForm(){
   contactForm.contactName.value = "";
   contactForm.contactEmail.value = "";
   contactForm.contactSubject.value = "";
   contactForm.contactMessage.value = "";
-  
-},false)
+}
